8af55ca205e84398921fb04a72739ba269eb4de6
Kien Hung Tran <kien.hung.tran@gmail.com>
2018-10-20 11:54:37 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
+
+// const logger = require('./logger');
+
+// function afficherNom(nom) {
+//     logger(nom);
+// }
+
+// console.log(logger);
+// afficherNom('kien');
+
---
+++ logger.js
@@ -0,0 +1,10 @@
+console.log(`file name is: ` + __filename);
+console.log(`dir name is: ` + __dirname);
+
+// var url = 'http://monlogger.io/log';
+
+// function log(message) {
+//     console.log(message);
+// }
+
+// module.exports = log;
